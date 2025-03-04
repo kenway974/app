239149525edd7ff7090a00f6534d0538ef8b7e99
kenway974 <kpignolet18@gmail.com>
2025-03-04 08:03:56 +0100

    relation user-task ajoutée

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -29,6 +29,10 @@ class Task
     #[ORM\Column]
     private ?bool $checked = null;
 
+    #[ORM\ManyToOne(inversedBy: 'tasks')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -90,6 +94,18 @@ class Task
     public function setChecked(bool $checked): static
     {
         $this->checked = $checked;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
 
         return $this;
     }
